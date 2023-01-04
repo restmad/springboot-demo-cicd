2c2bc5dc74ee16caad985343b46fa090f88fec61
restmad <restmad@outlook.com>
2023-01-05 00:44:37 +0800

    update staff

---
+++ springboot-demo-cicd/src/main/java/com/example/springbootdemocicd/SpringbootDemoCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoCicdApplication.class, args);
+    }
+
+}
---
+++ springboot-demo-cicd/src/test/java/com/example/springbootdemocicd/SpringbootDemoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
